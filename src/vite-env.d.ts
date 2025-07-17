14dd4a4e3d1f4460019bd0ebd51b6757dfdb3c56
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-07-17 17:23:35 -0300

    refactor: config do tooltip

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+declare module '@tiptap/react';
+declare module '@tiptap/starter-kit';
+declare module '@tiptap/extension-image';
+declare module '@tiptap/extension-link';
