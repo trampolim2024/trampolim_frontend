42de91680791f84a614ed1aadad78e356983c0fe
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-07-17 17:23:59 -0300

    feat: novas rotas adicionadas

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import EntrepreneurPage from './components/pages/entrepreneur/Entrepreneur.tsx';
 import About from './components/pages/about-trampolim/About.tsx';
 import SignUpPage from './components/pages/sign-up/SignupPage.tsx';
 import LoginPage from './components/pages/login/LoginPage.tsx';
+import PlatformAdmPage from './components/pages/platform-adm/PlatformAdmPage.tsx';
+import PlatformEntrepreneurPage from './components/pages/platform-entrepreneur/PlatformEntrepreneurPage.tsx';
+import PlatformReviewerPage from './components/pages/platform-reviewer/PlatformReviewerPage.tsx';
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
         <Route path="/sobre" element={<About />} />
         <Route path="/cadastro" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/logado-adm" element={<PlatformAdmPage />} />
+        <Route path="/logado-empreendedor" element={<PlatformEntrepreneurPage />} />
+        <Route path="/logado-avaliador" element={<PlatformReviewerPage />} />
       </Routes>
     </Router>
   );
